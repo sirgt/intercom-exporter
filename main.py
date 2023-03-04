c02d343bf88ce9f5dd775d3edaf478a7cf256ee0
Leonel Reyes <lreyes6@gmail.com>
2023-03-04 12:48:07 -0600

    fix: fixing looped loop

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ head = {
     }
 
 conv_url = "https://api.intercom.io/conversations"
-pages = "?per_page=15"
+pages = "?per_page=150"
 
 
 def conversation_details(headers, page_url, conv_id):
@@ -25,23 +25,32 @@ def conversation_details(headers, page_url, conv_id):
 def main(headers, page_url, per_page):
     url = page_url + per_page
     response = requests.get(url, headers=headers).json()
-    for key, value in response.items():
-        if isinstance(value, list):
-            for item in value:
-                for k, v in item.items():
-                    if k == 'id':
-                        #conversation_details(head, page_url, v)
 
+    for key, value in response.items():
         if isinstance(value, dict):
             for k, v in value.items():
-                if k == 'next':
-                    starting_after = "&starting_after=" + v['starting_after']
-                    print(page_url + per_page + starting_after)
+                if k == 'total_pages':
+                    total_pages = v
 
-        if isinstance(value, str):
-            print(f'Value is a string: {value}')
+    for i in range(total_pages):
+        print(url)
+        response = requests.get(url, headers=headers).json()
+
+        for key, value in response.items():
+            if isinstance(value, dict):
+                for k, v in value.items():
+                    if k == 'next':
+                        starting_after = \
+                            "&starting_after=" + v['starting_after']
+                        url = page_url + per_page + starting_after
+
+            if isinstance(value, list):
+                for item in value:
+                    for k, v in item.items():
+                        if k == 'id':
+                            print(v)
+                            conversation_details(head, page_url, v)
 
 
 if __name__ == '__main__':
     main(head, conv_url, pages)
-
